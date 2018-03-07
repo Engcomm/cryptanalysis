0dfc24bb3c985e6f2bf366264f391aafc803ba5d
Rowayda-Khayri <rowayda_khayri@yahoo.com>
2018-03-07 18:12:19 +0200

    encrypt the message

--- cryptanalysis.py
+++ cryptanalysis.py
@@ -1,4 +1,6 @@
 
+#### Encryption ####
+
 #take msg input
 
 msg = raw_input("Enter a msg to be encrypted")
@@ -14,4 +16,22 @@ print shift
 # letters array
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+cipherText = ""
+
+
+# encryption
+
+for i in range(0,len(msg)): # loop over all msg characters
+
+    index = letters.index(msg[i]) #get index of character in letters
+    print index
+
+    cipherText += letters[index+int(shift)]
+
+    print cipherText
+
+
+
+
 
