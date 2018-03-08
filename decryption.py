9bc08fb185b5c3b25adb16dfcce5ea68b4b820a2
Rowayda-Khayri <rowayda_khayri@yahoo.com>
2018-03-08 16:49:45 +0200

    calculate each letter's frequency percentage

--- decryption.py
+++ decryption.py
@@ -14,19 +14,27 @@ print ciphertext
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-# get frequency of each letter in ciphertext  -> use nltk
+## get frequency of each letter in ciphertext
 
-lettersFrequency = []
+lettersFrequency = [] # to store each letter frequency in ciphertext
 
+# calculate each letter frequency
 for letter in letters :
     lettersFrequency.append(ciphertext.count(letter))
 
 print lettersFrequency
 
+#define function to calculate each letter's frequency percentage
 def percentage(count, total):
     return 100 * float(count) / total
 
-print percentage(ciphertext.count('a'), len(ciphertext))
+lettersFrequencyPercentages = [] #to store each letter's frequency percentage
+
+#calculate each letter's frequency percentage
+for letter in letters:
+    lettersFrequencyPercentages.append(percentage(ciphertext.count(letter), len(ciphertext)))
+
+print lettersFrequencyPercentages
 
 
 
